99366ec9ca457d0b5085edfa3e321258edfbb4d4
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-04 15:24:54 +0800

    var splitchar is there, as of now the syntax is r-g-b[-a]

--- main.js
+++ main.js
@@ -13,19 +13,31 @@ var app = new Vue({
     methods: {
         updateColor(color) {
 
-            if (color.indexOf(',') >= 0) {
-                var colorsplit = color.split(',')
+            // THE CHAR FOR SPLITTING
+            var splitchar = '-'
+            
+            // CHECK IF SYNTAX IS CORRECT
+            if (color.indexOf(splitchar) >= 0) {
 
+                // SPLIT ACCORDING TO SPLITCHAR AND RETURNS ARRAY
+                var colorsplit = color.split(splitchar)
+
+                // CONSOLE LOG THE SPLITTED LENGTH
                 console.log(colorsplit.length)
 
+                // IF LENGTH IS == 3 (IN OTHER WORDS, ITS RGB)
                 if(colorsplit.length == 3){
 
+                    // VAR RGB TO GIVE TO THE JQUERY CSS
                     var rgb = 'rgb(' + colorsplit[0] + ',' + colorsplit[1] + ',' + colorsplit[2] + ')'
 
+                    // APPLY THE BG COLOR WITH RGB
                     $("body").css("background-color", rgb)
 
+                    // LOG THE STRING OUT
                     console.log(rgb)
 
+                    // PUSH IT TO AN ARRAY
                     this.history.push(rgb)
                     
                 }
