e526f5e6959a66d4c9274fb5c20950a6f1c0ab9d
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-04 14:56:50 +0800

    Input now needs color format as r,g,b and not rgb(r,g,b)

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 var app = new Vue({
     el: ".app",
 
-    data(){
-        return{
+    data() {
+        return {
             test: "hey",
             input: "",
+
+            history: []
         }
     },
 
     methods: {
-        updateColor(){
-            console.log(this.input)
-            document.getElementById("body").style.backgroundColor = this.input
+        updateColor(color) {
+
+            if (color.indexOf(',') >= 0) {
+                var colorsplit = color.split(',')
+                var inputcolor = 'rgb(' + colorsplit[0] + ',' + colorsplit[1] + ',' + colorsplit[2] + ')'
+                console.log(inputcolor)
+                $("body").css("background-color", inputcolor)
+                this.input = ""
+                if (this.history.indexOf(color) < 0) {
+                    this.history.push(color)
+                }
+            }else{
+                this.input = "Wrong!"
+            }
+
+
+
+            
+
+
+
         }
     }
 })
